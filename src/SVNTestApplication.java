ab66fa1d8676a4a92c20f88309e60f8866ae9a4c
viorel <viorel.bibiloiu@esolutions.ro>
2017-10-16 16:29:10 +0300

    Bug fix

--- src/SVNTestApplication.java
+++ src/SVNTestApplication.java
@@ -2,6 +2,6 @@
 
 public class SVNTestApplication {
 	public static void main(String[] args) {
-		System.out.println("hello!!");
-	}
+		System.out.println("Hello Git, from branch bug#1234 !!");
+		}
 }
